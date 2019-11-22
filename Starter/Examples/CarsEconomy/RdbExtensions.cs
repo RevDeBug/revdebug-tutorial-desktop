2a6d741b26522f252652098bf7c780b60c7fdfd6
Adam Kruszewski <adam@revdebug.com>
2019-11-22 15:41:33 +0100

    Add example custom serialization rules

---
+++ Starter/Examples/CarsEconomy/RdbExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+#region NoTimeTravel
+namespace Starter.Examples.CarsEconomy
+{
+    public static class RdbExtensions
+    {
+        public static string RdbSerialize(this System.Xml.Linq.XElement obj)
+        {
+            return obj.Name+": "+obj.Value;
+        }
+    }
+}
+#endregion
